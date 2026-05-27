ce33cde833e638ead8d3b4901291cd12f4ff21c7
Oct072021 <1299424192@qq.com>
2026-05-27 10:11:44 +0800

    feat: 本地路径解析反斜杠

--- src/parseProject/parseLocalProject.js
+++ src/parseProject/parseLocalProject.js
@@ -2,7 +2,8 @@ import path from 'path';
 import fs from 'fs';
 
 export async function parseLocalProject(projectRoot) {
-  const packageJsonPath = path.join(projectRoot, 'package.json');
+  const normalizedProjectRoot = projectRoot.replace(/\\/g, '/');
+  const packageJsonPath = path.join(normalizedProjectRoot, 'package.json');
   const json = await fs.promises.readFile(packageJsonPath, 'utf8');
   return JSON.parse(json);
 }
